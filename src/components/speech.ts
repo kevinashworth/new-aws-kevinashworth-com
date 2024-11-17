5954885c659520b4c14cbe920a11379ddd61edad
Kevin Ashworth <kevinashworth@yahoo.com>
2024-11-17 09:06:44 -0800

    Add blog post with First Letter Tool
    Added xs screen size to the site for the buttons on this tool

---
+++ src/components/speech.ts
@@ -0,0 +1,18 @@
+// const barbie_speech =
+//   "It is literally impossible to be a woman. You are so beautiful, and so smart, and it kills me that you don't think you're good enough. Like, we have to always be extraordinary, but somehow we're always doing it wrong.\n\n" +
+//   "You have to be thin, but not too thin. And you can never say you want to be thin. You have to say you want to be healthy, but also you have to be thin. You have to have money, but you can't ask for money because that's crass. You have to be a boss, but you can't be mean. You have to lead, but you can't squash other people's ideas. You're supposed to love being a mother, but don't talk about your kids all the damn time. You have to be a career woman but also always be looking out for other people.\n\n" +
+//   "You have to answer for men's bad behavior, which is insane, but if you point that out, you're accused of complaining. You're supposed to stay pretty for men, but not so pretty that you tempt them too much or that you threaten other women because you're supposed to be a part of the sisterhood.\n\n" +
+//   "But always stand out and always be grateful. But never forget that the system is rigged. So find a way to acknowledge that but also always be grateful.\n\n" +
+//   "You have to never get old, never be rude, never show off, never be selfish, never fall down, never fail, never show fear, never get out of line. It's too hard! It's too contradictory and nobody gives you a medal or says thank you! And it turns out in fact that not only are you doing everything wrong, but also everything is your fault.\n\n" +
+//   "I'm just so tired of watching myself and every single other woman tie herself into knots so that people will like us. And if all of that is also true for a doll just representing women, then I don't even know.";
+
+// const brokovich_speech =
+//   "Yeah. Yeah, sexy, huh? How 'bout this for a number? Six. That's how old my other daughter is, eight is the age of my son, two is how many times I've been married - and divorced; sixteen is the number of dollars I have in my bank account. 850-3943. That's my phone number, and with all the numbers I gave you, I'm guessing zero is the number of times you're gonna call it.";
+
+const a_few_good_speech =
+  "You can’t handle the truth! Son, we live in a world that has walls. And those walls have to be guarded by men with guns. Who’s gonna do it? You? You, Lt. Weinberg? I have a greater responsibility than you can possibly fathom. You weep for Santiago and you curse the Marines. You have that luxury. You have the luxury of not knowing what I know: that Santiago’s death, while tragic, probably saved lives. And my existence, while grotesque and incomprehensible to you, saves lives."; //\n\n" +
+// "You don’t want the truth. Because deep down, in places you don’t talk about at parties, you want me on that wall. You need me on that wall. We use words like honor, code, loyalty ... we use these words as the backbone to a life spent defending something. You use ’em as a punchline. " +
+// "I have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom I provide, then questions the manner in which I provide it! I’d rather you just said thank you and went on your way. Otherwise, I suggest you pick up a weapon and stand a post. " +
+// "Either way, I don’t give a damn what you think you’re entitled to!";
+
+export default a_few_good_speech;
