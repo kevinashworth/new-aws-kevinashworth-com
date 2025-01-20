4664216dedc503e3f3f8eddb31b5ef35d403fe70
Kevin Ashworth <kevinashworth@yahoo.com>
2025-01-20 15:33:10 -0800

    refactor: remove unused interfaces and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,17 +45,6 @@ export default defineConfig({
     icon({
       include: {
         tabler: ["*"],
-        "flat-color-icons": [
-          "template",
-          "gallery",
-          "approval",
-          "document",
-          "advertising",
-          "currency-exchange",
-          "voice-presentation",
-          "business-contact",
-          "database",
-        ],
       },
     }),
     ...whenExternalScripts(() =>
