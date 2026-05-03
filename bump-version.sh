ee526c6b59c01ae2984f8a10347a1e2895bdf42b
Kevin Ashworth <kevinashworth@yahoo.com>
2026-05-03 12:22:17 -0700

    update version to 2.3.7

--- bump-version.sh
+++ bump-version.sh
@@ -7,5 +7,5 @@ BUMP_TYPE=${1:-patch}
 npm --no-git-tag-version version "$BUMP_TYPE"
 npx genversion --es6 --double --semi src/version.ts
 git add package.json package-lock.json src/version.ts
-version=$(grep -oP "export const version = '\K[^']*" src/version.ts)
+version=$(sed -n 's/export const version = "\([^"]*\)";/\1/p' src/version.ts)
 git commit -m "update version to $version"
